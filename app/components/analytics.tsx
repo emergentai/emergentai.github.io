d3cf1e512b0b3c4405c1c9fa3c8d3484525a0fde
Dashvin Singh <circleline10@gmail.com>
2024-12-11 18:54:08 -0500

    user next Script tags instead

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,9 +1,21 @@
 "use client";
+import Script from "next/script";
 
 export function Analytics() {
 	return (
 		<>
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-YNYXN5PL55"></script>
+		<Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YNYXN5PL55"
+        />
+		<Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${'G-YNYXN5PL55'});
+          `}
+        </Script>
 		</>
 		
 	);
