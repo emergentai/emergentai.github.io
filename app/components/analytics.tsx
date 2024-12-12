8fce094e4b798838518e8d14d9718d863f0d1d0c
Dashvin Singh <circleline10@gmail.com>
2024-12-11 23:17:02 -0500

    user npm library for google analytics

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,22 +1,9 @@
 "use client";
-import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export function Analytics() {
 	return (
-		<>
-		<Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-YNYXN5PL55"
-        />
-		<Script id="google-analytics">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', ${'G-YNYXN5PL55'});
-          `}
-        </Script>
-		</>
-		
+		<GoogleAnalytics gaId="G-YNYXN5PL55"/>
 	);
 }
